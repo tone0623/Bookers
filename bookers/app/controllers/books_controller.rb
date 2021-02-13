e2dced62e4a9793f31282ec28d3a68a15f11682d
tone0623 <tone0623@icloud.com>
2021-02-13 05:13:47 +0000

    [Finish]bookers

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -1,34 +1,49 @@
 class BooksController < ApplicationController
 
+  #投稿一覧表示
   def index
     @books = Book.all
     @book = Book.new
   end
 
+  #新規投稿
   def create
-    book = Book.new(book_params)
-    if book.save
+    @books = Book.all
+    @book = Book.new(book_params)
+    
+    #バリデーションチェック
+    if @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to book_path(book)
+      redirect_to book_path(@book)
+    else
+      render :index
     end
   end
 
+  #詳細画面表示
   def show
     @book = Book.find(params[:id])
   end
 
+  #編集画面表示
   def edit
     @book = Book.find(params[:id])
   end
-  
+
+  #編集反映
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    
+    #バリデーションチェック
+    if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
-      redirect_to book_path(book)
+      redirect_to book_path(@book)
+    else
+      render :edit
     end
   end
   
+  #投稿削除
   def destroy
     book = Book.find(params[:id])
     if book.destroy
@@ -36,7 +51,8 @@ class BooksController < ApplicationController
       redirect_to books_path
     end
   end
-
+  
+  #ストロングパラメータ
   private
   def book_params
     params.require(:book).permit(:title, :body)
